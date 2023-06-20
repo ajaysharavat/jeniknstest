4910a8863bfb75bde21d53aea082d5627abf440e
ajay Sharavat <ajaysharavat.miet@gmail.com>
2023-06-20 13:00:32 +0000

    jenkin test

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 
 <?php echo "jenkin test"; ?>
 <?php
-echo "this is jenkins project with jenkins auto pulls" 
+echo "this is jenkins project with jenkins auto pulls";
+echo "new commit"; 
+echo "testting";
+echo "this is new";
+echo "welcome to ajayshatatavata teyeydbdb";
+echo "this is ajay saharavat";
 ?>
 
